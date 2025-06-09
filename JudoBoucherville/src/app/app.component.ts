1cf75efae1a175eac1c502cb7e5b7e586684a3f3
hugolevacher <hugolevacher13@gmail.com>
2025-06-09 14:59:30 -0400

    Setup and global styles

--- JudoBoucherville/src/app/app.component.ts
+++ JudoBoucherville/src/app/app.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'JudoBoucherville';
+  title = 'Judo Boucherville';
+
+  onActivate(event: any) {
+    window.scroll(0, 0);
+  }
 }
